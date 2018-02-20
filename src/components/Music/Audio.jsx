ecaad93c132dbf49aadf1dce386822518bdcc6e4
Matthew Aderhold <matthewaderhold@gmail.com>
2018-02-20 13:40:30 -0600

    Deploying to GitHub Pages

--- src/components/Music/Audio.jsx
+++ src/components/Music/Audio.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import BattleAudioOGG from './audio_dependancies/128-battle_vs_gym_leader_.ogg'
 import BattleAudioMP3 from './audio_dependancies/128-battle (vs gym leader).mp3'
+import BattleAudioWEBM from './audio_dependancies/128_battle__vs_gym_leader_.webm'
 import MenuSelectOGG from './audio_dependancies/300-Menu_Select.ogg'
 import MenuSelectMP3 from './audio_dependancies/300-Menu_Select.mp3'
 import DamageOGG from './audio_dependancies/301-damage-sfx.ogg'
@@ -15,7 +16,8 @@ export default class Audio extends Component {
     render() {
         let OGG;
         let MP3;
-        let battle = () => { OGG = BattleAudioOGG; MP3 = BattleAudioMP3 }
+        let WEBM;
+        let battle = () => { OGG = BattleAudioOGG; MP3 = BattleAudioMP3; WEBM = BattleAudioWEBM }
         let menuSelect = () => { OGG = MenuSelectOGG; MP3 = MenuSelectMP3 }
         let damage = () => OGG = DamageOGG; // MP3 = DamageMP3
         let victory = () => OGG = VictoryOGG; // MP3 = VictoryMP3
@@ -33,6 +35,7 @@ export default class Audio extends Component {
                     <audio autoPlay loop>
                         <source src={OGG} type="audio/ogg" />
                         <source src={MP3} type="audio/mp3" />
+                        <source src={WEBM} type="audio/webm" />
                     </audio>
                 </div>
 
